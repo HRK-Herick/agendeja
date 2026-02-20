89b14acc75c0107035628c00519598d1c38b4fc8
Herick Pereira Ferreira <rm100783@estudante.fieb.edu.br>
2026-02-20 20:00:59 -0300

    first commit

---
+++ src/main/java/com/agendamentos/agendeja/model/entity/Usuario.java
@@ -0,0 +1,16 @@
+package com.agendamentos.agendeja.model.entity;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String password;
+    private String area_atuacao;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String cnpj;
+
+}
---
+++ src/test/java/com/agendamentos/agendeja/AgendejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamentos.agendeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
